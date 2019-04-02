bcbe2dcaaf4a03805a815275a2412a9e522f6b31
Sebastian Guzman <sg35@princeton.edu>
2019-04-01 20:10:38 -0400

    preferences added

--- proj_333/match/models.py
+++ proj_333/match/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Preferences(models.Model):
+    pref1 = models.PositiveSmallIntegerField(default=0)
+    pref2 = models.PositiveSmallIntegerField(default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
