e3217b0e4b77f6719bda10c2fa60777d8cc8ee42
Sebastian Guzman <sg35@princeton.edu>
2019-04-12 13:43:00 -0400

    deployed on Heroku

--- proj_333/proj_333/settings.py
+++ proj_333/proj_333/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*$-9diy)^jl=kc@ctq&l*v2h6egc)!ft3vf5%j(_3bidc#p0k='
+# SECRET_KEY = '*$-9diy)^jl=kc@ctq&l*v2h6egc)!ft3vf5%j(_3bidc#p0k='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coffeeconnection.herokuapp.com']
 
 
 # Application definition
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -129,3 +133,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
--- proj_333/users/models.py
+++ proj_333/users/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    preferred_name = models.TextField(max_length=None, default = "Jane Doe")
+    preferred_name = models.TextField(max_length=None, default="Jane Doe")
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    personal_message = models.TextField(max_length=None, default = "I am excited to meet you")
+    personal_message = models.TextField(max_length=None, default="I am excited to meet you")
     matchedMateID = models.PositiveIntegerField(default=0)
-    matchedMatePName = models.TextField(max_length=None, default = "John Doe")
+    matchedMatePName = models.TextField(max_length=None, default="John Doe")
     matchedMateImage = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    matchedMatePMessage = models.TextField(max_length=None, default = "Meet me please!!!")
-    matchedMatePEmail = models.TextField(max_length=None, default = "johndoe@gmail.com")
+    matchedMatePMessage = models.TextField(max_length=None, default="Meet me please!!!")
+    matchedMatePEmail = models.TextField(max_length=None, default="johndoe@gmail.com")
 
     def __str__(self):
         return f'{self.user.username} Profile'
