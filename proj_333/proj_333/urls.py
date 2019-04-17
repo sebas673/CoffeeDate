80c8597ac73fd962c5c77677c27def5ae5730152
Sebastian Guzman <sg35@princeton.edu>
2019-04-16 22:16:40 -0400

    added CAS, but broke everything else :(

--- proj_333/proj_333/urls.py
+++ proj_333/proj_333/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +32,7 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('', include('match.urls')),
+    url('accounts/', include('uniauth.urls.cas_only', namespace='uniauth'))
 ]
 
 if settings.DEBUG:
