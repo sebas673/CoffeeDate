44358a4430a97f10f001b0c962a5b0e39a85e100
Sebastian Guzman <sg35@princeton.edu>
2019-05-06 16:56:36 -0400

    added matching algorithm and email links

--- proj_333/match/views.py
+++ proj_333/match/views.py
@@ -17,6 +17,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 
 
+from itertools import permutations 
+
+
 def home(request):
     if request.user.is_authenticated:
         user = request.user
@@ -163,13 +166,11 @@ class PrefsUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 # @login_required
 # @user_passes_test(lambda u: u.is_superuser)
 def match_all(request):  
-    is_group = False
-    pk = -1
-
-    matching = random_match(is_group, pk)
+    users = [user for user in User.objects.all().filter(Profile__is_matched='False')] # Profile needs to change to Prefs?
+    matching = full_match(users)
     for match in matching:
-        user1, user2 = User.objects.get(id=match[0]), User.objects.get(id=match[1])
-        set_match(user1, user2)
+        set_match(match[0], match[1])
+        set_match(match[1], match[0])
     return render(request, 'match/home.html')
 
 
@@ -183,11 +184,12 @@ def match_group(request, pk):
         for pair in pairs:
             pair.delete()
 
-        is_group = True
+        member_list = [Group.members.all() for Group in Group.objects.all().filter(id=pk)]
+        group_users = [Profile.user for Profile in member_list[0]]
 
-        matching = random_match(is_group, pk)
+        matching = full_match(group_users)
         for match in matching:
-            user1, user2 = User.objects.get(id=match[0]), User.objects.get(id=match[1])
+            user1, user2 = User.objects.get(id=match[0].id), User.objects.get(id=match[1].id)
             pair = Pair(pair_1=user1.id, pair_1_first=user1.first_name, pair_1_last=user1.last_name,
                         pair_2=user2.id, pair_2_first=user2.first_name, pair_2_last=user2.last_name, pair_group=group_in)
             pair.save()
@@ -201,6 +203,74 @@ def match_group(request, pk):
         return redirect('group-detail', pk)
 
 
+# matching function
+def full_match(users):
+    
+    # get preference/no preference user lists
+    pref_users = [user for user in users if user.Profile.prefs_match==True]
+    rand_users = [user for user in users if user not in pref_users]
+    
+    # call helper functions
+    pref_matching_even, pref_remainder = pref_match_helper(pref_users)
+    rand_matching_even, rand_remainder = rand_match_helper(rand_users)
+    
+    # do something with pref_remainder
+    return pref_matching_even + rand_matching_even
+
+
+# random matching helper function
+def rand_match_helper(users):
+
+    # if there are 0 users
+    if len(users) == 0: return [], []
+    
+    # shuffle users
+    random.shuffle(users)
+    
+    # separate users into even #, remainder
+    highest_even = (len(users) // 2) * 2
+    users_even = users[:highest_even]
+    users_remainder = users[highest_even:]
+    
+    # get matching
+    matching = get_match(users_even)
+
+    return matching, users_remainder
+
+
+# preference-based matching helper function
+def pref_match_helper(users):
+
+    # if there are 0 users
+    if len(users) == 0: return [], []
+    
+    # separate users into even #, remainder
+    highest_even = (len(users) // 2) * 2
+    users_even = users[:highest_even]
+    users_remainder = users[highest_even:]
+    
+    # fetch user preferences
+    preferences = {}
+    for user in users_even: preferences[user.id] = fetch_prefs(user)
+    
+    # get all permutations of users
+    perms = list(permutations(users_even))
+
+    # variables for minimum-dist matching
+    min_dist = 1000000
+    min_matching = []
+
+    # iterate over permutations of users, find minimum-dist matching
+    for perm in perms:
+        perm = list(perm)
+        matching = get_match(perm)
+        distance = get_match_dist(matching, preferences)
+        if distance < min_dist: min_matching, min_dist = matching, distance
+    
+    return min_matching, users_remainder
+
+
+# sets user 2 as user 1 match
 def set_match(user1, user2):
 
     user1.Profile.mate_ID = user2.id
@@ -212,38 +282,79 @@ def set_match(user1, user2):
     user1.Profile.is_matched = True
     user1.Profile.save()
 
-    user2.Profile.mate_ID = user1.id
-    user2.Profile.mate_firstname = user1.first_name
-    user2.Profile.mate_lastname = user1.last_name
-    user2.Profile.mate_personal_message = user1.Profile.personal_message
-    user2.Profile.mate_image = user1.Profile.image
-    user2.Profile.mate_email = user1.email
-    user2.Profile.is_matched = True
-    user2.Profile.save()
+
+# fetches user preferences
+def fetch_prefs(user):
+
+    prefs = np.zeros(5)
+    prefs[0] = user.Prefs.pref1
+    prefs[1] = user.Prefs.pref2
+    prefs[2] = user.Prefs.pref3
+    prefs[3] = user.Prefs.pref4
+    prefs[4] = user.Prefs.pref5
+    
+    return prefs
 
 
-# if is_group is True then match all the members of the group with pk
-def random_match(is_group, pk):  
-    pairs = []
+# fetches matching for given ordering of users
+def get_match(user_list):
+    
+    # set up matching list
+    matching = []
 
-    if is_group == False:
-        user_ids = [user.id for user in User.objects.all().filter(Profile__is_matched='False')]
-
-    elif is_group == True:
-        member_list = [Group.members.all() for Group in Group.objects.all().filter(id=pk)]
-        user_ids = [Profile.user.id for Profile in member_list[0]]
-
-    random.shuffle(user_ids)
-
-    i = 0
-    j = 0
-    numUsers = len(user_ids)
+    # add pairs
+    i, numUsers = 0, len(user_list)
     while i < numUsers - 1:
-        pairs.append([user_ids[i], user_ids[i + 1]])
+        matching.append([user_list[i], user_list[i + 1]])
         i += 2
 
-    # odd number of users case
-    if numUsers % 2 == 1:
-        pairs.append([user_ids[0], user_ids[numUsers - 1]])
+    return matching
 
-    return pairs
+
+# get average distance between pairs in matching
+def get_match_dist(matching, prefs):
+
+    matching_dist = 0
+    for match in matching:
+        pref1, pref2 = prefs[match[0].id], prefs[match[1].id]
+        match_dist = np.linalg.norm(pref2 - pref1)**2
+        matching_dist += match_dist
+
+    return matching_dist/len(matching)
+
+
+
+
+
+
+
+
+
+
+
+# # matches people from the group identified by pk
+# def match_group(request, pk):  
+#     group_in = Group.objects.get(id=pk)
+#     if request.user == group_in.owner:
+
+#         # will delete all the old Pairs when running a new matching
+#         pairs = Pair.objects.all().filter(pair_group=group_in)
+#         for pair in pairs:
+#             pair.delete()
+
+#         is_group = True
+
+#         matching = random_match(is_group, pk)
+#         for match in matching:
+#             user1, user2 = User.objects.get(id=match[0]), User.objects.get(id=match[1])
+#             pair = Pair(pair_1=user1.id, pair_1_first=user1.first_name, pair_1_last=user1.last_name,
+#                         pair_2=user2.id, pair_2_first=user2.first_name, pair_2_last=user2.last_name, pair_group=group_in)
+#             pair.save()
+
+#         # context = {
+#         #     'pairs': Pair.objects.all().filter()
+#         # }
+
+#         return redirect('group-detail', pk)
+#     else:
+#         return redirect('group-detail', pk)
